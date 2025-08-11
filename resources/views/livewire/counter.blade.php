293cc64671c035fcc0405e6c1d1f62b2b223238d
Vincent Neil Valenzona <sneil8323@gmail.com>
2025-08-11 10:06:06 +0800

    Update counter.blade.php

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,9 +1,20 @@
-<div class="p-6">
-    <h1 class="text-2xl font-bold mb-4">Counter</h1>
+<div class="flex flex-col items-center justify-center min-h-screen space-y-6">
+    <h1 class="text-3xl font-bold">Count: {{ $count }}</h1>
 
-    <div class="flex items-center space-x-4">
-        <button wire:click="decrement" class="px-4 py-2 bg-red-500 text-white rounded">-</button>
-        <span class="text-xl">{{ $count }}</span>
-        <button wire:click="increment" class="px-4 py-2 bg-green-500 text-white rounded">+</button>
+    <div class="space-x-4">
+        <button
+            wire:click="increment"
+            class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition"
+        >
+            +
+        </button>
+
+        <button
+            wire:click="decrement"
+            class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+            @if($count === 0) disabled class="opacity-50 cursor-not-allowed" @endif
+        >
+            -
+        </button>
     </div>
 </div>
